63421d92d6896c40c37aa6d53d170e0d14971c2f
Alex <pich.alex01@gmail.com>
2020-12-07 13:23:53 -0800

    Space some layout

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
 
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+`;
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
+
 export default function Layout({ children }) {
   return (
-    <div>
+    <>
       <GlobalStyles />
       <Typography />
-      <Nav />
-      {children}
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+        </ContentStyles>
+      </SiteBorderStyles>
       <Footer />
-    </div>
+    </>
   );
 }
