7f4ba44fb461750d8bc5eb2e515d7e70fd99eb39
Alex <pich.alex01@gmail.com>
2020-12-07 20:44:34 -0800

    Grid styles for Presets

--- gatsby/src/components/PresetList.js
+++ gatsby/src/components/PresetList.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PresetGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 200px;
+`;
+
+const PresetStyles = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--row, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2 {
+    font-size: 2.4rem;
+  }
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 function SinglePreset({ preset }) {
   return (
-    <div>
+    <PresetStyles>
       <Link to={`/preset/${preset.slug.current}`}>
         <h2>
-          <span className="mark">{preset.name}</span>
+          <span>{preset.name}</span>
         </h2>
         <p>{preset.tags.map((tag) => tag.name)}</p>
-        <Img fixed={preset.image.asset.fixed} alt={preset.name} />
+        <Img fluid={preset.image.asset.fluid} alt={preset.name} />
       </Link>
-    </div>
+    </PresetStyles>
   );
 }
 
 export default function PresetList({ presets }) {
   return (
-    <div>
+    <PresetGridStyles>
       {presets.map((preset) => (
         <SinglePreset preset={preset} key={preset.id} />
       ))}
-    </div>
+    </PresetGridStyles>
   );
 }
